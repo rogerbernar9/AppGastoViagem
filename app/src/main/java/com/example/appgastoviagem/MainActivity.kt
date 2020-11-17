6737cadecd43a60535a5c079184ae3b223eac0d6
rogerbernar9 <roger.bernar9@gmail.com>
2020-11-17 14:19:35 -0300

    eventos de click

--- app/src/main/java/com/example/appgastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/appgastoviagem/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.appgastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+
+    }
+
+    private fun calculate() {
+
     }
 }
