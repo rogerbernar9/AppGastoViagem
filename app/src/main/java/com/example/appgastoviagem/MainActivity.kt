7b551f27484d8ccc9de5f07c2f6345a3d4f789a6
rogerbernar9 <roger.bernar9@gmail.com>
2020-11-17 15:20:58 -0300

    finalizando o app

--- app/src/main/java/com/example/appgastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/appgastoviagem/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.appgastoviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun calculate() {
+        if (validation()) {
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+                val valorFinal = (distance * price) / autonomy
+                totalValue.text = "R$ ${"%.2f".format(valorFinal)}"
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, getString(R.string.valoresValidos), Toast.LENGTH_LONG).show()
+            }
 
+        } else
+            Toast.makeText(this, getString(R.string.notificacao), Toast.LENGTH_LONG).show()
+    }
+
+    private fun validation(): Boolean {
+        val distance = editDistance.text.toString()
+        val price = editPrice.text.toString()
+        val autonomy = editAutonomy.text.toString()
+        if (distance == "" || price == "" || autonomy == "") {
+            return false
+        }
+        return true
     }
 }
